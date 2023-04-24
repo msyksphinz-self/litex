b67143ddb10f8cd4b8fcbbda0a6cadaf75f77f4c
msyksphinz <msyksphinz.dev@gmail.com>
2023-04-24 09:27:33 +0900

    Fix litex_setup for Scariv

--- litex_setup.py
+++ litex_setup.py
@@ -128,7 +128,6 @@ git_repos = {
     "pythondata-cpu-serv":         GitRepo(url="https://github.com/litex-hub/"),
     "pythondata-cpu-vexriscv":     GitRepo(url="https://github.com/litex-hub/"),
     "pythondata-cpu-vexriscv-smp": GitRepo(url="https://github.com/litex-hub/", clone="recursive"),
-    "pythondata-cpu-scariv":       GitRepo(url="https://github.com/msyksphinz-self/", clone="recursive"),
 }
 
 # Installs -----------------------------------------------------------------------------------------
@@ -214,8 +213,8 @@ def litex_setup_init_repos(config="standard", tag=None, dev_mode=False):
             # Clone Repo.
             print_status(f"Cloning {name} Git repository...")
             repo_url = repo.url
-            # if dev_mode:
-            #     repo_url = repo_url.replace("https://github.com/", "git@github.com:")
+            if dev_mode:
+                repo_url = repo_url.replace("https://github.com/", "git@github.com:")
             subprocess.check_call("git clone {url} {options}".format(
                 url     = repo_url + name + ".git",
                 options = "--recursive" if repo.clone == "recursive" else ""
